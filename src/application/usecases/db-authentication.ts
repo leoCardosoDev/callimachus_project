8fd3655b5b25a9548a3ab284d0274e622d1f32e9
leoCardosoDev <leocardosodev@gmail.com>
2023-12-25 10:55:31 +0000

    feat: ensure DbAuthentication calls HashCompare with correct values

--- src/application/usecases/db-authentication.ts
+++ src/application/usecases/db-authentication.ts
@@ -1,10 +1,17 @@
 import { Authentication } from '@/domain/usecases'
-import { LoadAccountByEmailRepository } from '../protocols'
+import { HashCompare, LoadAccountByEmailRepository } from '@/application/protocols'
 
 export class DbAuthentication implements Authentication {
-  constructor (private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) {}
-  async auth (authenticationParams: Authentication.Params): Promise<any> {
-    await this.loadAccountByEmailRepository.loadByEmail(authenticationParams.email)
+  constructor (
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    private readonly hashComparer: HashCompare
+  ) {}
+
+  async auth (authenticationParams: Authentication.Params): Promise<Authentication.Result | null> {
+    const account = await this.loadAccountByEmailRepository.loadByEmail(authenticationParams.email)
+    if (account) {
+      await this.hashComparer.compare(authenticationParams.password, account.password)
+    }
     return null
   }
 }
--- tests/application/mocks/mock-db-account.ts
+++ tests/application/mocks/mock-db-account.ts
@@ -1,8 +1,13 @@
 import { LoadAccountByEmailRepository } from '@/application/protocols'
+import faker from 'faker'
 
 export class LoadAccountByEmailRepositorySpy implements LoadAccountByEmailRepository {
   email: string
-  result: LoadAccountByEmailRepository.Result | null = null
+  result: LoadAccountByEmailRepository.Result | null = {
+    id: faker.datatype.uuid(),
+    name: faker.name.findName(),
+    password: faker.internet.password()
+  }
 
   async loadByEmail (email: string): Promise<LoadAccountByEmailRepository.Result | null> {
     this.email = email
